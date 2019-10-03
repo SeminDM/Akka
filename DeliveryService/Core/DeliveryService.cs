c3e44e4b792aed8c4848600b59755b3d03c9e5a5
Osipov Michail <ossup0v@gmail.com>
2019-10-03 16:56:58 +0300

    Support notifications via WebSockets (SignalR)

--- DeliveryService/Core/DeliveryService.cs
+++ DeliveryService/Core/DeliveryService.cs
@@ -1,6 +1,7 @@
 ﻿using DeliveryApi;
 using System;
 using System.IO;
+using System.Threading;
 
 namespace DeliveryCore
 {
@@ -9,16 +10,21 @@ namespace DeliveryCore
         public DeliveryResult DeliverGoods(DeliveryData data)
         {
             var rand = new Random();
+            var secondsCount = rand.Next(0, 5);
+            Thread.Sleep(TimeSpan.FromSeconds(secondsCount));
             var now = DateTime.Now;
-            var path = @"E:\delivery.txt";
+            var path = @"C:\Users\mosip\Documents\temp\delivery.txt";
             var randomNumber = rand.Next(1, 100);
             var success = randomNumber % 2 == 0;
+            var waitedTime = $"you was waited {secondsCount} seconds";
 
             var msg = success
-                ? $"Goods {string.Join(",", data.Goods)} are delivered by {data.ShipId} at {now}"
-                : $"Delivery was failed at {now}";
-
-            File.WriteAllText(path, msg);
+                ? $"Goods {string.Join(",", data.Goods)} are delivered by {data.ShipId} at {now}, {waitedTime}"
+                : $"Delivery was failed at {now}, {waitedTime}";
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLineAsync(msg);
+            }
             return new DeliveryResult { Address = $"{randomNumber} Baker street", DeliveryDate = now, IsSuccess = success };
         }
     }
--- NotificationService/Core/NotificationService.cs
+++ NotificationService/Core/NotificationService.cs
@@ -7,14 +7,14 @@ namespace NotificationCore
     {
         public void NotifyAboutDeliveryStart(DeliveryStartNotification data)
         {
-            var path = @"E:\notify.txt";
+            var path = @"C:\Users\mosip\Documents\temp\notify.txt";
             var msg = $"Goods will be delivered by {data.TransportType} {data.ShipId}";
             File.WriteAllText(path, msg);
         }
 
         public void NotifyAboutDeliveryFinish(DeliveryFinishNotification data)
         {
-            var path = @"E:\notify.txt";
+            var path = @"C:\Users\mosip\Documents\temp\notify.txt";
             var msg = data.IsSuccess 
                 ? $"Your goods are delivered by {data.ShipId} successfully"
                 : $"Delivery of your goods was failed";
