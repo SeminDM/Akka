3a94239a1f0c866b9889c42db4fa8c1af73f584b
Osipov Michail <ossup0v@gmail.com>
2019-10-09 16:06:39 +0300

    extra spaces removed

--- DeliveryService/Api/Messages.cs
+++ DeliveryService/Api/Messages.cs
@@ -5,18 +5,18 @@ namespace DeliveryApi
     public class DeliveryResult
     {
         public TransportType TransportType { get; }
-        public string ShipId               { get; }
-        public DateTime DeliveryDate       { get; }
-        public string Address              { get; }
-        public bool IsSuccess              { get; set; }
+        public string ShipId { get; }
+        public DateTime DeliveryDate { get; }
+        public string Address { get; }
+        public bool IsSuccess { get; set; }
 
         public DeliveryResult(TransportType TransportType, string ShipId, DateTime DeliveryDate, string Address, bool IsSuccess = false)
         {
-            this.TransportType  = TransportType;
-            this.ShipId         = ShipId;
-            this.DeliveryDate   = DeliveryDate;
-            this.Address        = Address;
-            this.IsSuccess      = IsSuccess;
+            this.TransportType = TransportType;
+            this.ShipId = ShipId;
+            this.DeliveryDate = DeliveryDate;
+            this.Address = Address;
+            this.IsSuccess = IsSuccess;
         }
     }
 
